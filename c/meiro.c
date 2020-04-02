0e9e6f6c8d4d2f452faefca4d1bebacaa27f66d4
aki-23 <sh-t_2.6-fp@ezweb.ne.jp>
2020-04-02 17:21:13 +0900

    Update meiro.c
    １次元配列に変更

--- c/meiro.c
+++ c/meiro.c
@@ -22,7 +22,7 @@ typedef struct
 } MazeBlock;
 
 //プレイヤー初期化
-int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN])
+int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW * MAZE_COLUMN])
 {
 	int i, j;
 
@@ -30,7 +30,7 @@ int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][M
 	{
 		for(j = 0; j < MAZE_COLUMN; j++)
 		{
-			if(maze[i][j].kind == START) //スタート地点ならばプレイヤーの位置に設定
+			if(maze[MAZE_COLUMN * i + j].kind == START) //スタート地点ならばプレイヤーの位置に設定
 			{
 				*playerRow = i;
 				*playerColumn = j;
@@ -43,7 +43,7 @@ int MazePlayerInit(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][M
 }
 
 //迷路表示
-void MazeDraw(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN])
+void MazeDraw(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW * MAZE_COLUMN])
 {
 	int i, j;
 
@@ -53,11 +53,11 @@ void MazeDraw(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COL
 		{
 			if(i == playerRow && j == playerColumn) //プレイヤー位置
 				printf("P");
-			else if(maze[i][j].flag == FALSE) //ブロックが判明していない場合
+			else if(maze[MAZE_COLUMN * i + j].flag == FALSE) //ブロックが判明していない場合
 				printf("?");
 			else
 			{
-				switch(maze[i][j].kind)
+				switch(maze[MAZE_COLUMN * i + j].kind)
 				{
 					case FRAME:
 						printf("□"); break; //枠
@@ -78,7 +78,7 @@ void MazeDraw(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COL
 enum MazeDirection{UP, DOWN, LEFT, RIGHT, Invalid};
 
 //プレイヤー移動
-void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN])
+void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW * MAZE_COLUMN])
 {
 	char buf[100];
 	int direction = -1;
@@ -106,8 +106,8 @@ void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][
 		{
 			if(*playerRow - 1 >= 1) //範囲外でないことを確認
 			{
-				maze[*playerRow - 1][*playerColumn].flag = TRUE;
-				if(maze[*playerRow - 1][*playerColumn].kind != WALL) //壁判定
+				maze[MAZE_COLUMN * ((*playerRow) - 1) + (*playerColumn)].flag = TRUE;
+				if(maze[MAZE_COLUMN * ((*playerRow) - 1) + (*playerColumn)].kind != WALL) //壁判定
 				{
 					*playerRow -= 1; //移動
 					printf("\n上に移動しました。\n");
@@ -125,8 +125,8 @@ void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][
 		{
 			if(*playerRow + 1 < MAZE_ROW - 1)
 			{
-				maze[*playerRow + 1][*playerColumn].flag = TRUE;
-				if(maze[*playerRow + 1][*playerColumn].kind != WALL)
+				maze[MAZE_COLUMN * ((*playerRow) + 1) + (*playerColumn)].flag = TRUE;
+				if(maze[MAZE_COLUMN * ((*playerRow) + 1) + (*playerColumn)].kind != WALL)
 				{
 					*playerRow += 1;
 					printf("\n下に移動しました。\n");
@@ -144,8 +144,8 @@ void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][
 		{
 			if(*playerColumn - 1 >= 1)
 			{
-				maze[*playerRow][*playerColumn - 1].flag = TRUE;
-				if(maze[*playerRow][*playerColumn - 1].kind != WALL)
+				maze[MAZE_COLUMN * (*playerRow) + ((*playerColumn) - 1)].flag = TRUE;
+				if(maze[MAZE_COLUMN * (*playerRow) + ((*playerColumn) - 1)].kind != WALL)
 				{
 					*playerColumn -= 1;
 					printf("\n左に移動しました。\n");
@@ -163,8 +163,8 @@ void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][
 		{
 			if(*playerColumn + 1 < MAZE_COLUMN - 1)
 			{
-				maze[*playerRow][*playerColumn + 1].flag = TRUE;
-				if(maze[*playerRow][*playerColumn + 1].kind != WALL)
+				maze[MAZE_COLUMN * (*playerRow) + ((*playerColumn) + 1)].flag = TRUE;
+				if(maze[MAZE_COLUMN * (*playerRow) + ((*playerColumn) + 1)].kind != WALL)
 				{
 					*playerColumn += 1;
 					printf("\n右に移動しました。\n");
@@ -180,9 +180,9 @@ void MazePlayerMove(int *playerRow, int *playerColumn, MazeBlock maze[MAZE_ROW][
 }
 
 //ゴール判定
-int MazeGoalCheck(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW][MAZE_COLUMN])
+int MazeGoalCheck(int playerRow, int playerColumn, MazeBlock maze[MAZE_ROW * MAZE_COLUMN])
 {
-	if(maze[playerRow][playerColumn].kind == GOAL)
+	if(maze[MAZE_COLUMN * playerRow + playerColumn].kind == GOAL)
 	{
 		printf("ゴール！\n");
 		return 1;
@@ -197,25 +197,25 @@ void MazeGame(int stage)
 	MazePlayer player;
 
 	//迷路
-	MazeBlock maze[STAGE][MAZE_ROW][MAZE_COLUMN] =
+	MazeBlock maze[STAGE][MAZE_ROW * MAZE_COLUMN] =
 	{
 		{ //STAGE0
-			{ {FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {WALL , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {PATH , FALSE} , {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE }, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE } },
+			{FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE },
+			{FRAME, TRUE } , {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {WALL , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {PATH , FALSE} , {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {WALL , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE }, {FRAME, TRUE },
+			{FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE },
 		},
 		{ //STAGE1
-			{ {FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE }, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {PATH , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE } },
-			{ {FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE } },
+			{FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE },
+			{FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {PATH , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {START, TRUE } , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {GOAL , TRUE }, {FRAME, TRUE },
+			{FRAME, TRUE } , {PATH , FALSE} , {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {WALL , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {PATH , FALSE} , {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {PATH , FALSE}, {FRAME, TRUE },
+			{FRAME, TRUE } , {FRAME, TRUE } , {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE }, {FRAME, TRUE },
 		}
 	};
 
